03440d36517c28047d90df6ae1abc8d2322483fe
clarisha octa viany phiranti <57120890+clarishaocta@users.noreply.github.com>
2019-11-30 16:40:17 +0700

    Add files via upload

---
+++ buzzbreak.php
@@ -0,0 +1,51 @@
+<?php 
+
+$c = "\e[1;36m"; 
+$g = "\e[1;32m"; 
+$r = "\e[0;31m"; 
+$pu = "\e[1;30m"; 
+
+$ { "GLOBALS" } ["ekibzjgsjjc_hiuwsxwnzxezmpmhn"] = "c"; 
+$ { "GLOBALS" } ["jpidbnjvustyw_xpskmyb_rckfavuy"] = "pu"; 
+$ { "GLOBALS" } ["sqswgxnr_ssdh_lw___ekz_rldngc_fi"] = "r"; 
+$ { "GLOBALS" } ["xjmehrcedgkta_sdjzrmz"] = "jum"; 
+$ { "GLOBALS" } ["eezdzdimnc_mzuqt_cbevsb_ay_ywz_rvnin"] = "g"; $ { "GLOBALS" } ["q_dgbsfntvho_yq_rzdbvjdo_"] = "b"; 
+$ { "GLOBALS" } ["zxkjwxsmfnsukjbitofljfs"] = "ch"; $ { "GLOBALS" } ["mleeagrdibzy_vnuqjsy_ompyxuzrvqqsrwk"] = "headers"; 
+$ { "GLOBALS" } ["aiaciyy_wqaifvxzpgconymls_quznrbpxobie"] = "data"; 
+$ { "GLOBALS" } ["jjpzs_thfetetvfxo_mxol_kefeqr_u_l"] = "data1"; 
+$ { "GLOBALS" } ["t_ini_f_ipqdybycledetikwwvy_"] = "result"; 
+$ { "GLOBALS" } ["mkvhtgipdizepmptzetiscrbleft"] = "k"; 
+$ { "GLOBALS" } ["vkolrvpzyzbqcyyfdzhpfy_fiikult"] = "no"; 
+
+echo " "; 
+system("clear"); 
+system("clear"); 
+system("clear"); 
+system("clear"); 
+
+system("figlet -f standard BuzzBreak |lolcat"); echo $ { $ { "GLOBALS" } ["ekibzjgsjjc_hiuwsxwnzxezmpmhn"] } . "========================================================= "; 
+echo $ { $ { "GLOBALS" } ["jpidbnjvustyw_xpskmyb_rckfavuy"] } . " Author By : Mr.M4xXX |_| Bot BuzzBreak \n"; 
+echo " MY Channel YT : MR.MINIMAX |_| FROM TERMUX ID3 \n"; 
+echo $ { $ { "GLOBALS" } ["ekibzjgsjjc_hiuwsxwnzxezmpmhn"] } . "==========================================================\n "; 
+echo " _____________________________________________ "; 
+echo $ { $ { "GLOBALS" } ["sqswgxnr_ssdh_lw___ekz_rldngc_fi"] } . " <<< [ MAAF BELUM BISA NAMPILIN BALLANCE NAMA DLL ] >>> \n"; 
+echo $ { $ { "GLOBALS" } ["ekibzjgsjjc_hiuwsxwnzxezmpmhn"] } . " <<< [ TAPI KALAU POINNYA NAMBAH BERARTI ITU WORK ] >>> \n"; 
+echo $ { $ { "GLOBALS" } ["sqswgxnr_ssdh_lw___ekz_rldngc_fi"] } . " <<< [ Kalau Sudah Limit Poinnya Nggak Nambah Lagi ] >>> \n"; 
+echo $ { $ { "GLOBALS" } ["ekibzjgsjjc_hiuwsxwnzxezmpmhn"] } . " <<< [ DAN KALIAN BISA LIHAT DIAPLIKASINYA ] >>> \n"; 
+echo " [_____________________________________________] \n"; 
+echo " "; $ { $ { "GLOBALS" } ["xjmehrcedgkta_sdjzrmz"] } = 50; 
+echo $ { $ { "GLOBALS" } ["eezdzdimnc_mzuqt_cbevsb_ay_ywz_rvnin"] } . "Mulai Mengkalim setiap 31 detik \n"; 
+echo "Diseason Ini Cuma Hanya Sampai \n" . $ { $ { "GLOBALS" } ["xjmehrcedgkta_sdjzrmz"] } . " Kali "; 
+echo "Karena Limitnya \n" . $ { $ { "GLOBALS" } ["xjmehrcedgkta_sdjzrmz"] } . " "; $ { $ { "GLOBALS" } ["q_dgbsfntvho_yq_rzdbvjdo_"] } = "https://api.buzzbreak.news/voyager/points/claim-reward"; include ('config.php'); function spin($b, $headers) { $ { $ { "GLOBALS" } ["zxkjwxsmfnsukjbitofljfs"] } = curl_init(); curl_setopt($ { $ { "GLOBALS" } ["zxkjwxsmfnsukjbitofljfs"] }, CURLOPT_URL, $ { $ { "GLOBALS" } ["q_dgbsfntvho_yq_rzdbvjdo_"] }); curl_setopt($ { $ { "GLOBALS" } ["zxkjwxsmfnsukjbitofljfs"] }, CURLOPT_FOLLOWLOCATION, true); curl_setopt($ { $ { "GLOBALS" } ["zxkjwxsmfnsukjbitofljfs"] }, CURLOPT_RETURNTRANSFER, 1); curl_setopt($ { $ { "GLOBALS" } ["zxkjwxsmfnsukjbitofljfs"] }, CURLOPT_HTTPHEADER, $ { $ { "GLOBALS" } ["mleeagrdibzy_vnuqjsy_ompyxuzrvqqsrwk"] }); $ { $ { "GLOBALS" } ["aiaciyy_wqaifvxzpgconymls_quznrbpxobie"] } = array("time_zone_offset" => "+07:00"); $ { $ { "GLOBALS" } ["jjpzs_thfetetvfxo_mxol_kefeqr_u_l"] } = json_encode($ { $ { "GLOBALS" } ["aiaciyy_wqaifvxzpgconymls_quznrbpxobie"] }, true); curl_setopt($ { $ { "GLOBALS" } ["zxkjwxsmfnsukjbitofljfs"] }, CURLOPT_POST, 1); curl_setopt($ { $ { "GLOBALS" } ["zxkjwxsmfnsukjbitofljfs"] }, CURLOPT_POSTFIELDS, $ { $ { "GLOBALS" } ["jjpzs_thfetetvfxo_mxol_kefeqr_u_l"] }); $ { $ { "GLOBALS" } ["t_ini_f_ipqdybycledetikwwvy_"] } = curl_exec($ { $ { "GLOBALS" } ["zxkjwxsmfnsukjbitofljfs"] }); $ { $ { "GLOBALS" } ["mkvhtgipdizepmptzetiscrbleft"] } = json_decode($ { $ { "GLOBALS" } ["t_ini_f_ipqdybycledetikwwvy_"] }, true); 
+echo "poin tambahanmu : " . $ { $ { "GLOBALS" } ["mkvhtgipdizepmptzetiscrbleft"] } ["points_earned"] . " "; } $ { $ { "GLOBALS" } ["vkolrvpzyzbqcyyfdzhpfy_fiikult"] } = 0; 
+
+while ($ { $ { "GLOBALS" } ["vkolrvpzyzbqcyyfdzhpfy_fiikult"] } < $ { $ { "GLOBALS" } ["xjmehrcedgkta_sdjzrmz"] }) { $ { $ { "GLOBALS" } ["vkolrvpzyzbqcyyfdzhpfy_fiikult"] } ++; 
+spin($ { $ { "GLOBALS" } ["q_dgbsfntvho_yq_rzdbvjdo_"] }, $ { $ { "GLOBALS" } ["mleeagrdibzy_vnuqjsy_ompyxuzrvqqsrwk"] }); sleep(31); } 
+
+
+?>
+
+
+
+
+
